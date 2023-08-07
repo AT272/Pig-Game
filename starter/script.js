e321493416a048ce5b8a8fa9b6ad0f1eefbb0595
Teodor Axinte <teodoraxinte1@gmail.com>
2023-08-07 12:19:41 +0300

    make roll dice btn work

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// selecting elements for future reference
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const imgDice = document.querySelector('.dice');
@@ -8,9 +10,48 @@ const current1El = document.querySelector('#current--1');
 const btnNewGame = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const player1Section = document.querySelector('.player--1');
+const player0Section = document.querySelector('.player--0');
+
+// initial conditions
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+imgDice.classList.add('hidden');
+
+// Roll button actions
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', () => {
+  // generate random dice
+
+  const randomDice = Math.ceil(Math.random() * 6);
+
+  // Display the dice image
+
+  imgDice.classList.remove('hidden');
+
+  imgDice.src = `dice-${randomDice}.png`;
+
+  // Check if dice is 1. If true switch to the next player
+
+  if (randomDice !== 1) {
+    currentScore += randomDice;
+    console.log(currentScore);
+    current0El.textContent = currentScore;
+  } else {
+    // Switch to next player
+    // a. change color theme
+    player0Section.classList.remove('player--active');
+    player1Section.classList.add('player--active');
+
+    // b.
+  }
+});
 
 btnNewGame.addEventListener('click', () => {
   score0El.textContent = 0;
   score1El.textContent = 0;
-  imgDice.style.display = 'none';
+  imgDice.classList.add('hidden');
 });
