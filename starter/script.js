17a544c93d6ceaf29ae6847a8ae81ec8cfeca894
Teodor Axinte <teodoraxinte1@gmail.com>
2023-08-08 20:59:29 +0300

    refactor and add finishing touches that were missed

--- starter/script.js
+++ starter/script.js
@@ -20,6 +20,7 @@ score1El.textContent = 0;
 imgDice.classList.add('hidden');
 
 //  Score variable declaration
+let playing = true;
 
 let currentScore = 0;
 
@@ -34,81 +35,95 @@ let switchPlayer = function () {
 // Roll button actions
 
 btnRoll.addEventListener('click', () => {
-  // generate random dice
+  if (playing) {
+    // generate random dice
 
-  const randomDice = Math.ceil(Math.random() * 6);
+    const randomDice = Math.ceil(Math.random() * 6);
 
-  // Display the dice image
+    // Display the dice image
 
-  imgDice.classList.remove('hidden');
+    imgDice.classList.remove('hidden');
 
-  imgDice.src = `dice-${randomDice}.png`;
+    imgDice.src = `dice-${randomDice}.png`;
 
-  // Check if dice is 1. If true switch to the next player
+    // Check if dice is 1. If true switch to the next player
 
-  if (randomDice !== 1) {
-    currentScore += randomDice;
+    if (randomDice !== 1) {
+      currentScore += randomDice;
 
-    // add the score to the urrent player if dice is not 1
+      // add the score to the urrent player if dice is not 1
 
-    if (player0Section.classList.contains('player--active')) {
-      current0El.textContent = currentScore;
+      if (player0Section.classList.contains('player--active')) {
+        current0El.textContent = currentScore;
+      } else {
+        current1El.textContent = currentScore;
+      }
     } else {
-      current1El.textContent = currentScore;
+      // Switch to next player
+      switchPlayer();
+      // a. change color theme through class manipulation
+      // if (player0Section.classList.contains('player--active')) {
+      //   player0Section.classList.remove('player--active');
+      // } else player0Section.classList.add('player--active');
+
+      // if (player1Section.classList.contains('player--active')) {
+      //   player1Section.classList.remove('player--active');
+      // } else player1Section.classList.add('player--active');
+
+      currentScore = 0;
+      current1El.textContent = 0;
+      current0El.textContent = 0;
     }
-  } else {
-    // Switch to next player
-    switchPlayer();
-    // a. change color theme through class manipulation
-    // if (player0Section.classList.contains('player--active')) {
-    //   player0Section.classList.remove('player--active');
-    // } else player0Section.classList.add('player--active');
-
-    // if (player1Section.classList.contains('player--active')) {
-    //   player1Section.classList.remove('player--active');
-    // } else player1Section.classList.add('player--active');
-
-    currentScore = 0;
-    current1El.textContent = 0;
-    current0El.textContent = 0;
   }
 });
 
 btnHold.addEventListener('click', () => {
-  // 1. Add current score to total score of active player
-  if (player0Section.classList.contains('player--active')) {
-    totalScore0 += currentScore;
-    score0El.textContent = totalScore0;
-    currentScore = 0;
-  } else {
-    totalScore1 += currentScore;
-    score1El.textContent = totalScore1;
-    currentScore = 0;
-  }
-  // 2. set surrent score to 0
-  // 3. swich to next player if score is < 100; else display winner
-  // debugger;
-  if (score0El.textContent < 100 && score1El.textContent < 100) {
-    //
-    switchPlayer();
+  if (playing) {
+    // 1. Add current score to total score of active player
+    if (player0Section.classList.contains('player--active')) {
+      totalScore0 += currentScore;
+      score0El.textContent = totalScore0;
+      currentScore = 0;
+    } else {
+      totalScore1 += currentScore;
+      score1El.textContent = totalScore1;
+      currentScore = 0;
+    }
+    // 2. set surrent score to 0
+    // 3. swich to next player if score is < 100; else display winner
+    // debugger;
+    if (score0El.textContent < 100 && score1El.textContent < 100) {
+      //
+      switchPlayer();
 
-    current0El.textContent = 0;
-    currentScore = 0;
-    current1El.textContent = 0;
-    //  else display winner
-  } else if (score0El.textContent >= 100) {
-    player0Section.classList.add('player--winner');
-  } else if (score1El.textContent >= 100)
-    player1Section.classList.add('player--winner');
+      current0El.textContent = 0;
+      currentScore = 0;
+      current1El.textContent = 0;
+      //  else display winner
+    } else if (score0El.textContent >= 100) {
+      player0Section.classList.add('player--winner');
+      playing = false;
+      imgDice.classList.add('hidden');
+    } else if (score1El.textContent >= 100) {
+      player1Section.classList.add('player--winner');
+      playing = false;
+      imgDice.classList.add('hidden');
+    }
+  }
 });
 
 btnNewGame.addEventListener('click', () => {
   score0El.textContent = 0;
   score1El.textContent = 0;
+  totalScore0 = 0;
+  totalScore1 = 0;
   currentScore = 0;
   current0El.textContent = 0;
   current1El.textContent = 0;
+  playing = true;
   imgDice.classList.add('hidden');
   player0Section.classList.toggle('player--active', true);
   player1Section.classList.toggle('player--active', false);
+  player0Section.classList.toggle('player--winner', false);
+  player1Section.classList.toggle('player--winner', false);
 });
